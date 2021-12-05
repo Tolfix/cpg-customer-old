273c53eafbdd47754f66e89708078c6b825eadfa
Matteus <tolfx@tolfix.com>
2021-12-05 19:45:44 +0100

    __layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "../app.css";
+    import { isAuth } from "../store";
+    import Login from "$lib/Login.svelte";
+    import SideBar from "../lib/Navigation/Sidebar.svelte"
+</script>
+
+{#if !$isAuth}
+<Login />
+{:else}
+    <SideBar>
+        <slot></slot>
+    </SideBar>
+{/if}
